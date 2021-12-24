f1ae070143398688cde15ab9281a580b21ff43bd
FighterOfKeyboard <johan1zsolt@gmail.com>
2021-12-24 16:43:24 +0100

    automatic image slider

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ var image = document.querySelector('.image');
 var bar = document.querySelector('.bar');
 var sideBar = document.querySelector('.side-bar');
 var sideBarList = document.querySelectorAll('.side-bar ul li');
+var i = 0;
+var images = new Array() // create new array to preload images
+images[0] = new Image() // create new instance of image object
+images[0].src = "1.png" // set image src property to image path, preloading image in the process
+images[1] = new Image()
+images[1].src = "2.png"
+images[2] = new Image()
+images[2].src = "3.png"
+
+var time = 5000;
+
 
 //renamed toggle function to toggleImage
 function toggleImage(e)
@@ -10,6 +21,31 @@ function toggleImage(e)
     
     image.src = source;
 }
+
+function autoslide()
+{
+    console.log("Function started");
+    console.log(i);
+     //if browser does not support the image object, exit.
+    if (!document.images){
+        return
+    }
+
+
+    document.getElementById("slide").src = images[i].src
+    
+
+    if (i < images.length -1) {
+        i++;
+    }
+    else{
+        i = 0;
+    }
+
+    setTimeout("autoslide()", time);
+}
+
+window.onload = autoslide;
 
 
 var j = 0;
